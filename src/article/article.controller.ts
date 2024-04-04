19e5eba7eee8981f43cc8225099b1bb88e21806e
Zehan Khan <zkhan@hootboard.com>
2024-04-05 01:21:50 +0530

    feat: :crossed_flags: create endpoint for route

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('article')
-export class ArticleController {}
+export class ArticleController {
+  // add article
+  @Post('/add')
+  addArticle(): string {
+    return 'This route handle to create article';
+  }
+
+  // get all article
+  @Get('/all')
+  getArticles(): string {
+    return 'This route handle to get all articles';
+  }
+
+  // add article
+  @Put('/update')
+  updateArticle(): string {
+    return 'This route handle to update article';
+  }
+
+  // add article
+  @Delete('/delete')
+  deleteArticle(): string {
+    return 'This route handle to delete article';
+  }
+}
