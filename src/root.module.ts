fc88250f47b8d5e239278abd220fb7742bff0f2e
Zehan Khan <zkhan@hootboard.com>
2024-04-05 00:37:42 +0530

    chore: :wastebasket: remove boilerplate code

---
+++ src/root.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class RootModule {
+  constructor() {
+    console.log('Root Module');
+  }
+}
