8eb7474c5a10458ef5fa2668ddfbc55bd18e627f
Zehan Khan <zkhan@hootboard.com>
2024-04-05 09:37:59 +0530

    feat: :memo: add doc of cli

--- command.sh
+++ command.sh
@@ -1,8 +1,20 @@
-# create nest app
-nest new app_name
-
-# generate module
-nest g mo user
-
-# generate controller
-nest g co article
+        name          │ alias       │ description                                  │
+      │ application   │ application │ Generate a new application workspace         │
+      │ class         │ cl          │ Generate a new class                         │
+      │ configuration │ config      │ Generate a CLI configuration file            │
+      │ controller    │ co          │ Generate a controller declaration            │
+      │ decorator     │ d           │ Generate a custom decorator                  │
+      │ filter        │ f           │ Generate a filter declaration                │
+      │ gateway       │ ga          │ Generate a gateway declaration               │
+      │ guard         │ gu          │ Generate a guard declaration                 │
+      │ interceptor   │ itc         │ Generate an interceptor declaration          │
+      │ interface     │ itf         │ Generate an interface                        │
+      │ library       │ lib         │ Generate a new library within a monorepo     │
+      │ middleware    │ mi          │ Generate a middleware declaration            │
+      │ module        │ mo          │ Generate a module declaration                │
+      │ pipe          │ pi          │ Generate a pipe declaration                  │
+      │ provider      │ pr          │ Generate a provider declaration              │
+      │ resolver      │ r           │ Generate a GraphQL resolver declaration      │
+      │ resource      │ res         │ Generate a new CRUD resource                 │
+      │ service       │ s           │ Generate a service declaration               │
+      │ sub-app       │ app         │ Generate a new application within a monorepo │
